aa1475f1c41117d60cfaab144c536882fdd3a728
northern-flower <78475347+northern-flower@users.noreply.github.com>
2022-09-09 15:32:08 +0200

    Add tests for check url spelling

--- TestWebsiteChecker.py
+++ TestWebsiteChecker.py
@@ -1,17 +1,75 @@
 #for unittesting
 import unittest
 
-#import class to test
+#for initialisation of connection
+import sqlite3
+
+#contains both WebsiteChecker and WebsiteCheckerUtils
 import WebsiteChecker
 
-#TODO: to merge TestWebsiteChecker and TestWebsiteCheckerDatabase
-
-class TestWebsiteChecker(unittest.TestCase) {
+class TestWebChecker(unittest.TestCase) {
 	
 	def _init_(self):
-		#TODO: initialise connection
+        connection = sqlite3.connect("our-database.db")
 	
-	def test_download_website_stats_google(self):
+    #
+    # url spelling true tests
+    #
+    #TODO: create a method to test those with parameters
+    #TODO: create one object to test on different methods
+    def check_url_spelling_true_simple_url():
+        wbutils = WebsiteChecker(connection)
+        self.assertEqual(wbutils.check_url_spelling("google.com"), true)
+    
+    def check_url_spelling_true_simple_url_with_www):
+        wbutils = WebsiteChecker(connection)
+        self.assertEqual(wbutils.check_url_spelling("www.goOgle.com"), true)
+    
+    def check_url_spelling_true_simple_url_with_http():
+        wbutils = WebsiteChecker(connection)
+        self.assertEqual(wbutils.check_url_spelling("http://1oogle.com"), true)
+    
+    def check_url_spelling_true_simple_url_with_https():
+        wbutils = WebsiteChecker(connection)
+        self.assertEqual(wbutils.check_url_spelling("https://goog-e.com"), true)
+    
+    def check_url_spelling_true_simple_url_with_256_characters():
+        wbutils = WebsiteChecker(connection)
+        self.assertEqual(wbutils.check_url_spelling("ulhxbtoarnqshozsuulpcztqstl.com"), true)
+
+    # url spelling false tests
+    def check_url_spelling_false_simple_url_1():
+        wbutils = WebsiteChecker(connection)
+        self.assertEqual(wbutils.check_url_spelling("www.goOgle."), false)
+    
+    def check_url_spelling_false_simple_url_2():
+        wbutils = WebsiteChecker(connection)
+        self.assertEqual(wbutils.check_url_spelling("goOgle-com"), false)
+    
+    def check_url_spelling_false_url_with_3_points_in_it():
+        wbutils = WebsiteChecker(connection)
+        self.assertEqual(wbutils.check_url_spelling("goOgle.com.nl"), false)
+    
+    def check_url_spelling_false_url_with_special_character():
+        wbutils = WebsiteChecker(connection)
+        self.assertEqual(wbutils.check_url_spelling("goOgle#.com"), false)
+    
+    def check_url_spelling_false_simple_url_with_more_than_256_characters():
+        wbutils = WebsiteChecker(connection)
+        self.assertEqual(wbutils.check_url_spelling("xpuhcxryawkyuxxztskjkuupecblwxal.com"), false)
+    
+    #
+    # check that ping is taking less time than the frequency ???
+    # ...
+    #
+    
+    #
+    # TODO: think about if i really need to test curl results ...
+    #
+    
+    # TODO: check the following tests:
+    
+    def test_download_website_stats_google(self):
 		websitechecker = WebsiteChecker(["google.com", 500, 0], connection)
 		self.assertEqual(websitechecker.download_website_stats(), true)
 	
@@ -28,53 +86,72 @@ class TestWebsiteChecker(unittest.TestCase) {
 		websitechecker = WebsiteChecker(["https://console.aiven.io/signup.html", 60, 1], connection)
 		self.assertEqual(websitechecker.download_website_stats(), true)
 
-}
+    
+    #TODO: agggregation tests
+    # ...
 
 
-#
-# verify that the entry has been inserted in the database
-#
-#https://medium.com/swlh/python-testing-with-a-mock-database-sql-68f676562461
-
-
-from mock_db import MockDB
-from mock import patch
-import utils
-
-class TestWebsiteCheckerDatabase(MockDB):
-	
-    def test_db_websites_insert(self):
-	with self.mock_db_config:
-		self.assertEqual(utils.db_write("""INSERT INTO `websites` (`primary_url`) VALUES
-                	('https://github.com/')"""), True)
-	
-    def test_db_websites_delete(self):
-	with self.mock_db_config:
-		self.assertEqual(utils.db_write("""DELETE FROM `websites` (`primary_url`) VALUES
-                	('https://github.com/')"""), True)
-	
-    def test_db_websites_insert_and_delete(self):
-        with self.mock_db_config:
-		self.assertEqual(utils.db_write("""INSERT INTO `websites` (`primary_url`) VALUES
-                	('https://github.com/')"""), True)
-            
-		self.assertEqual(utils.db_write("""INSERT INTO `websites` (`primary_url`) VALUES
-                        ('https://github.com/')"""), False)
-			
-		self.assertEqual(utils.db_write("""DELETE FROM `websites` (`primary_url`) VALUES
-                        ('https://github.com/')"""), True)
-            
-		self.assertEqual(utils.db_write("""DELETE FROM `websites` (`primary_url`) VALUES
-                	('https://github.com/')"""), False)
-			
-			
-	# TODO: there will be similar tests for 'website_stats' and 'website_content'
-	# TODO: for 'website_content' it would make sense to verify timeout while writing to the database			
-			
-			
 #
 # other test ideas
 #
 # verify if there is an infinite loop
 # how many insertions in the database have been made during a certain time
 #
+
+    
+    
+    
+    
+    
+}
+
+
+#
+#TODO: incorporate this
+#
+
+
+from mock_db import MockDB
+from mock import patch
+
+#TODO: may be handle exceptions ???
+#TODO: probably need to be initialised to know in which database to write
+import utils
+
+#for initialisation of connection
+import sqlite3
+
+class TestWebCheckerDatabase(MockDB):
+	
+    def _init_(self):
+		connection = sqlite3.connect("our-database.db")
+    
+    def test_db_websites_insert(self):
+		with self.mock_db_config:
+			self.assertEqual(utils.db_write("""INSERT INTO `websites` (`primary_url`) VALUES
+                            ('https://test.com/')"""), True)
+	
+	def test_db_websites_delete(self):
+		with self.mock_db_config:
+			self.assertEqual(utils.db_write("""DELETE FROM `websites` (`primary_url`) VALUES
+                            ('https://test.com/')"""), True)
+	
+    def test_db_websites_insert_and_delete(self):
+        with self.mock_db_config:
+			self.assertEqual(utils.db_write("""INSERT INTO `websites` (`primary_url`) VALUES
+                            ('https://test.com/')"""), True)
+            
+			self.assertEqual(utils.db_write("""INSERT INTO `websites` (`primary_url`) VALUES
+                            ('https://test.com/')"""), False)
+			
+			self.assertEqual(utils.db_write("""DELETE FROM `websites` (`primary_url`) VALUES
+                            ('https://test.com/')"""), True)
+            
+			self.assertEqual(utils.db_write("""DELETE FROM `websites` (`primary_url`) VALUES
+                            ('https://test.com/')"""), False)
+			
+			
+	# TODO: there will be similar tests for 'website_stats' and 'website_content'
+	# TODO: for 'website_content' it would make sense to verify timeout while writing to the database			
+			
+			
