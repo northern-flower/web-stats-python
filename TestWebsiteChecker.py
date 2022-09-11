14839300afb35dbcd2f3fb31db6d078aa52ed560
northern-flower <78475347+northern-flower@users.noreply.github.com>
2022-09-11 14:07:38 +0200

    Update TestWebsiteChecker.py

--- TestWebsiteChecker.py
+++ TestWebsiteChecker.py
@@ -7,17 +7,20 @@ import sqlite3
 #contains both WebsiteChecker and WebsiteCheckerUtils
 import WebsiteChecker
 
+#constants
+PING_PERFORMANCE = 1000
+
 class TestWebChecker(unittest.TestCase) {
 	
-    def setUp(self):
+	def setUp(self):
         connection = sqlite3.connect("our-database.db")
         wbutils = WebsiteChecker(connection)
-    
-    #
+	
+	#
     # url spelling true tests
     #
     
-    #TODO: consider using pytest instead of unittest (check the advantages)
+    #TOCONSIDER: consider using pytest instead of unittest (check the advantages)
     def test_url_spelling_true_simple_url():
         self.assertEqual(wbutils.check_url_spelling("google.com"), true, "google.com should be a valid url")
     
@@ -55,43 +58,43 @@ class TestWebChecker(unittest.TestCase) {
     # we could use them instead of writing our own
     #
     
-    #
-    # TODO: about ping - may be additional test that it doesn't take too much time ?
-    #
-    
-    #
-    # TODO: think about if i really need to test curl results ...
-    #
-    
-    
+    #TOIMPROVE:
     #get_curl_results
     #retrieve_url_id_in_websites_list(url)
     #test expected raises/exceptions - create constants for them
     
-    
-    #TODO: probably put this into a separate integration file
-    
+    #integration tests (or is it end to end)
     def test_download_website_stats_google(self):
 		websitechecker = WebsiteChecker(["google.com", 500, 0], connection)
 		self.assertEqual(websitechecker.download_website_stats(), true)
+        #check ping < 1000 for example
+        #check the last line in the database
+        #check that the content is not downloaded
+        #check the log file
 	
 	def test_download_website_stats_localhost(self):
 		websitechecker = WebsiteChecker(["localhost", 300, 1], connection)
 		self.assertEqual(websitechecker.download_website_stats(), true)
-        # TODO: do a select and check that saved line in the database contains ping < 1000 ms for example
-        # select last row 
-        # ...
+        #check ping < 1000 for example
+        #check the last line in the database
+        #check that the content is not downloaded
+        #check the log file
 	
 	def test_download_website_stats_broken_url_format(self):
 		websitechecker = WebsiteChecker(["localhost.", 30, 0], connection)
 		self.assertEqual(websitechecker.download_website_stats(), false)
+        #check ping < 1000 for example
+        #check the last line in the database
+        #check that the content is not downloaded
+        #check the log file
 	
 	def test_download_website_stats_broken_url(self):
 		websitechecker = WebsiteChecker(["https://console.aiven.io/signup.html", 60, 1], connection)
 		self.assertEqual(websitechecker.download_website_stats(), true)
-        # TODO: do a select and check that saved line in the database contains code 403
-        # select last row 
-        # ...
+        #check ping < 1000 for example
+        #check the last line in the database
+        #check that the content is not downloaded
+        #check the log file
     
     # verify that logger is producing something (no infinite loop)
     # verify that the number of entrance into the database is an expected number
@@ -100,71 +103,86 @@ if __name__ == '__main__':
     unittest.main()
 
 #
-#TODO: incorporate this
+#TOIMRPOVE: incorporate this into the previous tests
 #
 
 from mock_db import MockDB
 from mock import patch
 import utils
+import time
 
 #for initialisation of connection
 import sqlite3
+
+#constants
+BLOB_INSERT_PERFORMANCE = 1000
+BLOB_DELETE_PERFORMANCE = 1000
 
 class TestWebCheckerDatabase(MockDB):
 	
     def _init_(self):
 		connection = sqlite3.connect("our-database.db")
     
+    # test insert and delete for 'websites' table
     def test_db_websites_insert_and_delete(self):
         with self.mock_db_config:
-	    self.assertEqual(utils.db_write("""INSERT INTO `websites` (`primary_url`) VALUES
+			self.assertEqual(utils.db_write("""INSERT INTO `websites` (`primary_url`) VALUES
                             (`https://test.com/`)"""), True, """First time insert of https://test.com/ into `websites` table should work""")
-            
-	    #
-            #TODO (optionally): make a select here to check if only 1 record has been inserted
-            #
             
             self.assertEqual(utils.db_write("""INSERT INTO `websites` (`primary_url`) VALUES
                             (`https://test.com/`)"""), False, """Second time insert of https://test.com/ into `websites` table should NOT work""")
 			
 			self.assertEqual(utils.db_write("""DELETE FROM `websites` where `primary_url` = `https://test.com/`"""), True, """First time delete of https://test.com/ from `websites` table should work""")
             
-	    #
-            #TODO (optionally): make a select here to check if only 1 record has been deleted
-            #
-            
             self.assertEqual(utils.db_write("""DELETE FROM `websites` where `primary_url` = `https://test.com/`"""), False, """Second time delete of https://test.com/ from `websites` table should NOT work""")
 	
+    # test insert and delete for 'website_stats' table
     def test_db_website_stats_insert_and_delete(self):
         with self.mock_db_config:
-			self.assertEqual(utils.db_write("""INSERT INTO `websites` (`primary_url`) VALUES
-                            ('https://test.com/')"""), True, """First time insert of https://test.com/ into `websites` table should work""")
-            
             self.assertEqual(utils.db_write("""INSERT INTO `website_stats` (`website_id`, `timestamp`, `http_response_time`, `status_code_returned`) VALUES
                             (1, 1594819641.9622827, 32, 0)"""), True, """First time insert of website_id = 1 (for timestamp in the past) into `website_stats` table should work""")
-            
-            #
-            #TODO (optionally): make a select here to check if only 1 record has been inserted
-            #
             
 			self.assertEqual(utils.db_write("""INSERT INTO `website_stats` (`website_id`, `timestamp`, `http_response_time`, `status_code_returned`) VALUES
                             (1, 1594819641.9622827, 545, 23)"""), False, """Second insertion for `website_stats` table should not be possible for the same `website_id` and `timestamp`""")
 			
 			self.assertEqual(utils.db_write("""DELETE FROM `website_stats` where website_id = 1 and timestamp = 1594819641.9622827""", True, """First time delete of website_id = 1 (in the past) from `website_stats` table should work""")
             
-            #
-            #TODO (optionally): make a select here to check if only 1 record has been deleted
-            #
-            
 			self.assertEqual(utils.db_write("""DELETE FROM `website_stats` where website_id = 1 and timestamp = 1594819641.9622827""", False, """Second time delete of website_id = 1 (in the past) from `website_stats` table should NOT work""")
-
-            self.assertEqual(utils.db_write("""DELETE FROM `websites` (`primary_url`) VALUES
-                            ('https://test.com/')"""), True, """First time delete of https://test.com/ from websites database should work""")
+    
+    # test insert and delete for 'website_content' table
+	# additional tests for insertion performace of the table (as it contains BLOB)
+	def test_db_website_content_insert_and_delete(self):
+        with self.mock_db_config:
+			
+            self.assertEqual(utils.db_write("""INSERT INTO `website_content` (`website_id`, `timestamp`, `downloaded_content`) VALUES
+                            (1, 1662890978.4920807, '<html></html>')"""), True, """First time insert of website_id = 1 (for timestamp in the past) into `website_content` table should work""")
             
-
-                            
+			self.assertEqual(utils.db_write("""INSERT INTO `website_content` (`website_id`, `timestamp`, `downloaded_content`) VALUES
+                            (1, 1662890978.4920807, '')"""), False, """Second insertion for `website_content` table should not be possible for the same `website_id` and `timestamp`""")
 			
-	# TODO: there will be similar tests for'website_content'
-	# TODO: for 'website_content' it would make sense to verify timeout while writing to the database			
+			self.assertEqual(utils.db_write("""DELETE FROM `website_content` where website_id = 1 and timestamp = 1662890978.4920807""", True, """First time delete of website_id = 1 (in the past) from `website_content` table should work""")
+            
+			self.assertEqual(utils.db_write("""DELETE FROM `website_content` where website_id = 1 and timestamp = 1662890978.4920807""", False, """Second time delete of website_id = 1 (in the past) from `website_content` table should NOT work""")
+            
+            #read a very large html from a file
+            f = open("very_large_html.html", "r")
+            
+            # BLOB insert performance
+            start = time.time()
+            
+            self.assertEqual(utils.db_write("""INSERT INTO `website_content` (`website_id`, `timestamp`, `downloaded_content`) VALUES
+                            (1, 1662890978.4920807, """ + f +""")"""), True, """(BLOB) First time insert of website_id = 1 (for timestamp in the past) into `website_content` table should work""" )
+            
+            end = time.time()
 			
+            self.assertEqual(end-start < BLOB_INSERT_PERFORMANCE, true, "Insert of BIG BLOB into `website_content` table should be possible under < " + BLOB_INSERT_PERFORMANCE + "ms")
+            
+            # BLOB delete performance
+            start = time.time()
+            
+            self.assertEqual(utils.db_write("""DELETE FROM `website_content` where website_id = 1 and timestamp = 1662890978.4920807""", True, """(BLOB) First time delete of website_id = 1 (in the past) from `website_content` table should work""")
+            
+            end = time.time()
 			
+            self.assertEqual(end-start < BLOB_DELETE_PERFORMANCE, true, "")
+            
